4090dfc656258ffd624fbb8e21afddf8ad23f694
Omar Morales <morales.ortega.omar@gmail.com>
2020-11-26 23:25:55 -0600

    first commit

---
+++ src/main/java/Producer.java
@@ -0,0 +1,19 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class Producer {
+
+    public Producer(){
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers","localhost:9092");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        ProducerRecord producerRecord = new ProducerRecord("academy2020", "name", "Hello Academy! ily");
+        KafkaProducer kafkaProducer = new KafkaProducer(properties);
+
+        kafkaProducer.send(producerRecord);
+        kafkaProducer.close();
+    }
+}
